f666ac74d76c1c3e9027a7b49aef63cdfb0dbd47
Richard Caceres <richard.caceres22590@outlook.com>
2019-08-07 09:48:06 -0400

    Updated to LSPDFR 4.3

--- AssortedCallouts/API/ComputerPlusFuncs.cs
+++ AssortedCallouts/API/ComputerPlusFuncs.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Rage;
-using ComputerPlus.API;
 using ComputerPlus;
+using ComputerPlus.API;
+
 
 namespace AssortedCallouts.API
 {
